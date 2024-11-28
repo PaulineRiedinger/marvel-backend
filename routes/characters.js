93aa4cb3cdd73629a5c225278ca64ab64187bb85
Pauline Riedinger <levron.pauline@gmail.com>
2024-11-28 19:40:50 +0100

    correction erreurs back-end

--- routes/characters.js
+++ routes/characters.js
@@ -1,35 +1,29 @@
-// Importer express et axios
 const express = require("express");
 const axios = require("axios");
 
-// Créer un routeur
 const router = express.Router();
 
-// Récupérer la clé de l'API
 const MARVEL_API_KEY = process.env.MARVEL_API_KEY;
 
-// Route GET pour récupérer la liste des personnages
 router.get("/", async (req, res) => {
   try {
-    let limit = 100;
-
+    let limit = req.query.limit || 100; // Par défaut, 100 résultats
     let filters = "";
+
     if (req.query.name) {
       filters += `&name=${req.query.name}`;
     }
-    if (req.query.limit) {
-      filters += `&limit=${req.query.limit}`;
-    }
     if (req.query.page) {
-      filters += `&skip=${(req.query.page - 1) * limit}`;
+      filters += `&offset=${(req.query.page - 1) * limit}`;
     }
-    // Requête GET pour récupérer les personnages
+
     const response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${MARVEL_API_KEY}${filters}&limit=${limit}`
     );
-    res.status(200).json(response.data); // Envoie les données reçues de l'API
+
+    res.status(200).json(response.data);
   } catch (error) {
-    // Si erreur -> message d'erreur
+    console.error("Erreur API Marvel :", error.response?.data || error.message);
     res.status(500).json({
       message:
         "Erreur lors de la récupération des personnages... C'est comme si Doctor Strange avait modifié la réalité !",
@@ -37,5 +31,4 @@ router.get("/", async (req, res) => {
   }
 });
 
-// Exporter le routeur
 module.exports = router;
